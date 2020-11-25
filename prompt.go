516124edd048091bcbf6e2ba9117e75a22a16389
Mark <git@happybeing.com>
2020-11-25 17:30:32 +0000

    Stub out call to terminal.ReadPassword() in prompt.go

--- prompt.go
+++ prompt.go
@@ -1,23 +1,19 @@
 package keyring
 
-import (
-	"fmt"
-	"os"
-
-	"golang.org/x/crypto/ssh/terminal"
-)
+import "errors"
 
 // PromptFunc is a function used to prompt the user for a password
 type PromptFunc func(string) (string, error)
 
 func terminalPrompt(prompt string) (string, error) {
-	fmt.Printf("%s: ", prompt)
-	b, err := terminal.ReadPassword(int(os.Stdin.Fd()))
-	if err != nil {
-		return "", err
-	}
-	fmt.Println()
-	return string(b), nil
+	return "", errors.New("gitportal-stub: keyring terminalPrompt() not supported")
+	// fmt.Printf("%s: ", prompt)
+	// b, err := terminal.ReadPassword(int(os.Stdin.Fd()))
+	// if err != nil {
+	// 	return "", err
+	// }
+	// fmt.Println()
+	// return string(b), nil
 }
 
 func fixedStringPrompt(value string) PromptFunc {
